20ff32353f863aff37b471da5a146c777e657280
Kit Westneat <kit.westneat@gmail.com>
2019-04-17 18:15:59 -0400

    covert more to ts

--- src/components/ads/ad-constants.ts
+++ src/components/ads/ad-constants.ts
@@ -3,27 +3,27 @@ export const AD_TYPES = {
    * The desktop mrecs are constrained by the box around it
    * Sizes: 300x50, 320x50, 300x250
    */
-  EMBEDDED_MREC: "EMBEDDED_MREC",
+  EMBEDDED_MREC: 'EMBEDDED_MREC',
 
   /**
    * The mobile mrecs are in-between boxes, so they are less constrained.
    * Sizes: 300x50, 320x50, 300x250, 300x600, 300x1050
    */
-  MOBILE_MREC: "MOBILE_MREC",
+  MOBILE_MREC: 'MOBILE_MREC',
 
   /**
    * The leaderboards are constrained by being next to the section title
    * header. Anything higher than 90  or wider than 728 looks silly.
    * Sizes: 728x90, 320x50, 300x50
    */
-  EMBEDDED_LEADERBOARD: "EMBEDDED_LEADERBOARD",
+  EMBEDDED_LEADERBOARD: 'EMBEDDED_LEADERBOARD',
 
   /**
    *  There are also leaderboards in-between modules that are not constrained
    * and can use the full width / height
    * Sizes: 728x90, 320x50, 300x50, 300x250, 970x250, 970x90
    */
-  SUPER_LEADERBOARD: "SUPER_LEADERBOARD",
+  SUPER_LEADERBOARD: 'SUPER_LEADERBOARD',
 
   /**
    *
@@ -39,8 +39,8 @@ export const AD_TYPES = {
    * < 728: 468x60, 300x250, 320x200, 320x50, 300x50, 300x600
    * < 468, 300x250, 320x200, 320x50, 300x50, 300x600
    */
-  SKYBOX: "SKYBOX",
-  FLOORBOARD: "FLOORBOARD",
+  SKYBOX: 'SKYBOX',
+  FLOORBOARD: 'FLOORBOARD'
 };
 
 /**
@@ -48,26 +48,24 @@ export const AD_TYPES = {
  * scripts
  */
 export const AD_FUNCS_MAP = {
-  [AD_TYPES.EMBEDDED_LEADERBOARD]: "pbh_new_fp_lboard",
-  [AD_TYPES.EMBEDDED_MREC]: "pbh_new_fp_mrec",
-  [AD_TYPES.MOBILE_MREC]: "pbh_new_fp_mobile_mrec",
-  [AD_TYPES.SUPER_LEADERBOARD]: "pbh_new_fp_superlboard",
-  [AD_TYPES.SKYBOX]: "pbh_new_fp_skybox",
-  [AD_TYPES.FLOORBOARD]: "pbh_new_fp_floorboard",
+  [AD_TYPES.EMBEDDED_LEADERBOARD]: 'pbh_new_fp_lboard',
+  [AD_TYPES.EMBEDDED_MREC]: 'pbh_new_fp_mrec',
+  [AD_TYPES.MOBILE_MREC]: 'pbh_new_fp_mobile_mrec',
+  [AD_TYPES.SUPER_LEADERBOARD]: 'pbh_new_fp_superlboard',
+  [AD_TYPES.SKYBOX]: 'pbh_new_fp_skybox',
+  [AD_TYPES.FLOORBOARD]: 'pbh_new_fp_floorboard'
 };
+
+export type AdSize = [number, number];
+
+type AdSizesMap = { [k: string]: AdSize[] };
 
 /**
  * This maps ad types to their corresponding list of potential sizes
  */
-export const AD_SIZES_MAP = {
+export const AD_SIZES_MAP: AdSizesMap = {
   [AD_TYPES.EMBEDDED_MREC]: [[300, 50], [320, 50], [300, 250]],
-  [AD_TYPES.MOBILE_MREC]: [
-    [300, 50],
-    [320, 50],
-    [300, 250],
-    [300, 600],
-    [300, 1050],
-  ],
+  [AD_TYPES.MOBILE_MREC]: [[300, 50], [320, 50], [300, 250], [300, 600], [300, 1050]],
   [AD_TYPES.EMBEDDED_LEADERBOARD]: [[728, 90], [320, 50], [300, 50]],
   [AD_TYPES.SUPER_LEADERBOARD]: [
     [728, 90],
@@ -75,7 +73,7 @@ export const AD_SIZES_MAP = {
     [300, 50],
     [300, 250],
     [970, 250],
-    [970, 90],
+    [970, 90]
   ],
   [AD_TYPES.SKYBOX]: [
     [728, 90],
@@ -85,7 +83,7 @@ export const AD_SIZES_MAP = {
     [300, 250],
     [468, 60],
     [970, 250],
-    [970, 90],
+    [970, 90]
   ],
   [AD_TYPES.FLOORBOARD]: [
     [728, 90],
@@ -95,6 +93,6 @@ export const AD_SIZES_MAP = {
     [300, 250],
     [468, 60],
     [970, 250],
-    [970, 90],
-  ],
+    [970, 90]
+  ]
 };
--- src/components/utils/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Responsive } from "./Responsive";
-export { ResponsiveProvider } from "./Responsive";
-
-export { default as LazyView } from "./LazyView";
