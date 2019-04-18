e318d32f608a090f270a2c336ce276aec7067dd6
Kit Westneat <kit.westneat@gmail.com>
2019-04-18 13:24:24 -0400

    fix grid error

---
+++ src/components/primitives/Grid/GridSlot.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+interface Props {
+  spacer?: number;
+  multiplier?: number;
+  itemsPerRow?: number;
+}
+
+export default class GridSlot extends React.PureComponent<Props> {
+  getStyles = () => {
+    const { itemsPerRow = 1, spacer = 10, multiplier = 1 } = this.props;
+
+    const styles = StyleSheet.create({
+      item: {
+        flexBasis: `${(100 / itemsPerRow) * multiplier}%`,
+        padding: spacer / 2
+      }
+    });
+
+    return styles.item;
+  };
+
+  public render() {
+    return <View style={this.getStyles()}>{this.props.children}</View>;
+  }
+}
