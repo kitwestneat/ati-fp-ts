c553f13db97a2d08b20e4422d2cf64d4b48a7a52
Kit Westneat <kit.westneat@gmail.com>
2019-07-02 17:31:10 -0400

    modify RecentAndTrending recent posts to be square images

--- src/components/modules/RecentAndTrending/RecentPostsGrid.tsx
+++ src/components/modules/RecentAndTrending/RecentPostsGrid.tsx
@@ -1,23 +1,27 @@
 import { createPostGrid } from '../shared/PostGrid';
 
 const RecentPostsGridDesktop = createPostGrid({
-  orderSpec: ['MD', 'MD', 'MD', 'MD', 'MD', 'MD', 'MD', 'MD'],
-  itemsPerRow: 2
+  orderSpec: ['SQ', 'SQ', 'SQ', 'SQ', 'SQ', 'SQ', 'SQ', 'SQ'],
+  itemsPerRow: 2,
+  postComponentAttrMap: {
+    SQ: {
+      layoutVariant: 'medium',
+      imageWidth: 250,
+      imageHeight: 250
+    }
+  }
 });
 
 const RecentPostsGridMobile = createPostGrid({
-    orderSpec: ['MD', 'MD', 'MD', 'MD', 'SM', 'SM', 'SM', 'SM'],
-    itemsPerRow: 1, 
-    postComponentAttrMap: {
-        MD: { layoutVariant: 'medium' }, 
-        SM : { 
-            layoutVariant: 'reduced', 
-            showImage: true 
-        }
+  orderSpec: ['MD', 'MD', 'MD', 'MD', 'SM', 'SM', 'SM', 'SM'],
+  itemsPerRow: 1,
+  postComponentAttrMap: {
+    MD: { layoutVariant: 'medium' },
+    SM: {
+      layoutVariant: 'reduced',
+      showImage: true
     }
+  }
 });
 
-export { 
-    RecentPostsGridDesktop, 
-    RecentPostsGridMobile 
-};
+export { RecentPostsGridDesktop, RecentPostsGridMobile };
