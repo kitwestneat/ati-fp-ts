d1d1de4100232a33fc81faaff81233b7677ed845
Kit Westneat <kit.westneat@gmail.com>
2019-07-17 16:27:48 -0400

    update phpify-fp.sh

--- phpify-fp.sh
+++ phpify-fp.sh
@@ -1,5 +1,26 @@
+#!/bin/bash
 IN=build/index.html
 OUT=build/enqueue.php
 
-(echo "<?php "; i=0; for x in $(grep -o '"wordpress/[^"]*\.js"' $IN | sed -e 's/wordpress//'); do echo "wp_enqueue_script('fp$i', $x);"; ((i++)); done) > $OUT
+get_scripts() {
+    i=0
+    for x in $(grep -o '"wordpress/[^"]*\.js"' $IN | sed -e 's/wordpress//'); do
+        echo "wp_enqueue_script('script-fp$i', $x); "
+        ((i++)) || true
+    done
+}
 
+get_styles() {
+    i=0
+    for x in $(grep -o '"wordpress/[^"]*\.css"' $IN | sed -e 's/wordpress//'); do
+        echo "wp_enqueue_style('style-fp$i', $x); "
+        ((i++)) || true
+    done
+}
+
+run() {
+    echo "<?php "
+    get_scripts
+    get_styles
+}
+run > $OUT
