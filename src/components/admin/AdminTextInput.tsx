692d839aa92fcd2df521d511e599cdb1f13c8a3e
Kit Westneat <kit.westneat@gmail.com>
2019-11-25 18:30:09 -0500

    Fix text input style

--- src/components/admin/AdminTextInput.tsx
+++ src/components/admin/AdminTextInput.tsx
@@ -4,13 +4,9 @@ import { TextInput, TextInputProps } from 'react-native';
 import styles from './styles';
 
 export default React.memo(function AdminTextInput({ style, ...rest }: TextInputProps) {
-  return (
-    <TextInput
-      {...rest}
-      style={{
-        ...styles.adminTextInput,
-        ...(style as object)
-      }}
-    />
-  );
+    const theStyle = [
+        styles.adminTextInput,
+        (style as object)
+    ];
+    return <TextInput {...rest} style={theStyle} />;
 });
